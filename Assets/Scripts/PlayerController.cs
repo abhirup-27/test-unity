58e9a1a2202a205ed3deaf71a25e69c717b13102
Abhirup Ghatak <abhirup.g27@gmail.com>
2022-06-13 13:47:07 +0530

    Chapter 7
    Chapter 7 - Assignment 1- Creating Game Loop: Final Task

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,13 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
 
     public Rigidbody2D rigidbody2d;
 
+    public GameObject gameWonPanel;
+    public GameObject gameLostPanel;
+
     public float speed;
+
+    private bool isGameOver = false;
+
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +25,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (isGameOver == true)
+        {
+            return;
+        }
+
         if (Input.GetAxis("Horizontal") > 0)// it is positive
         {
             rigidbody2d.velocity = new Vector2(speed, 0f);
@@ -43,7 +55,25 @@ public class PlayerController : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("Level Complete !!!");
+        if (other.tag == "Door")
+        {
+            Debug.Log("Level Complete !!!");
+            gameWonPanel.SetActive(true);
+            isGameOver = true;
+        }
+        else if (other.tag == "Enemy")
+        {
+            Debug.Log("Level Lost !!!");
+            gameLostPanel.SetActive(true);
+            isGameOver = true;
+
+        }
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Debug.Log("Button Clicked");
     }
 }
 
